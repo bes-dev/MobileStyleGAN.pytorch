f3c0244617381b926f001a07dcfe6bb647862b73
Sergei Belousov <belbes122@yandex.ru>
2021-08-13 12:31:12 +0300

    Update compare.py

--- compare.py
+++ compare.py
@@ -10,7 +10,7 @@ from core.model_zoo import model_zoo
 def main(args):
     cfg = load_cfg(args.cfg)
     distiller = Distiller(cfg)
-    if args.ckpt is not None:
+    if args.ckpt is not "":
         ckpt = model_zoo(args.ckpt)
         load_weights(distiller, ckpt["state_dict"])
 
